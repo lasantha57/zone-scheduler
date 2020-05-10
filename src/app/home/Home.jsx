339910849648c2ad0de7b5cefdab098f5edcf662
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-10 23:30:36 +0800

    add schedule list with search

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Schedulelist from './ScheduleList';
+
 const Home = () => {
 
     const [searchTerm, setSearchTerm] = React.useState('');
@@ -7,6 +9,7 @@ const Home = () => {
     return (
         <div className="container">
             <input type="search" className="form-control mb-3" placeholder="Search by Zone" value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} />
+            <Schedulelist searchTerm={searchTerm}></Schedulelist>
         </div>
     )
 }
