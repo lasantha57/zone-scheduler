f0c3d3fa8437545b870a3194469d2487e4e2d1c6
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-10 23:27:56 +0800

    remove axios

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import "./reset.css";
-import "./index.css";
+import React from 'react';
+import { render } from 'react-dom';
+import './reset.css';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import App from "./app/App";
+import App from './app/App';
 
-import axios from 'axios';
-import { BASE_API } from './utils/config';
-
-axios.defaults.baseURL = BASE_API;
-
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
