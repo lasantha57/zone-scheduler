b7520443d1fcfd1bc6ec8ac8bb789c8ce53486cc
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-10 15:35:41 +0800

    setup tests

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+
+describe('<App />', () => {
+
+  it('App renders without crashing', () => {
+    shallow(<App />);
+  });
+
+
+});
