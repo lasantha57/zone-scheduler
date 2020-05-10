1a0e085754159ceb64b30c1e50566f00e5ebc4df
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-11 02:47:59 +0800

    Updates

---
+++ precache-manifest.7eef78644c4686e1c57fe601a973e935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef5cca1828f2ab0333470dc5123e28e",
+    "url": "/zone-scheduler/index.html"
+  },
+  {
+    "revision": "a35a3d00c001c0bebab1",
+    "url": "/zone-scheduler/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "645d4a87f22daee0f519",
+    "url": "/zone-scheduler/static/css/main.9efe0913.chunk.css"
+  },
+  {
+    "revision": "a35a3d00c001c0bebab1",
+    "url": "/zone-scheduler/static/js/2.fb80b0f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zone-scheduler/static/js/2.fb80b0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645d4a87f22daee0f519",
+    "url": "/zone-scheduler/static/js/main.0f9495a6.chunk.js"
+  },
+  {
+    "revision": "bdca6a66651b3716a7b3",
+    "url": "/zone-scheduler/static/js/runtime-main.7e5c4fc9.js"
+  }
+]);
