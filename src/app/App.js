5cbedbcdd14b43f3fe355eb9b5f75fa01e78d813
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-11 01:37:42 +0800

    refactor header

--- src/app/App.js
+++ src/app/App.js
@@ -28,9 +28,7 @@ const App = () => {
     <div className="container">
       <Router>
         <GlobalProvider>
-          <header>
-            <Header></Header>
-          </header>
+          <Header></Header>
           <main>
             {renderRoutes()}
           </main>
--- src/app/shared/header/Header.jsx
+++ src/app/shared/header/Header.jsx
@@ -16,21 +16,21 @@ const Header = () => {
     }
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <NavLink className="navbar-brand" to="/">Zone Scheduler</NavLink>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
-                <div className="navbar-nav">
-                    <Link to={`/create`}><button className="btn btn-outline-secondary mr-2">Create Schedule</button></Link>
-                </div>
-                <div className="btn-group btn-group ml-2" role="group" >
-                    <button type="button" className={scale === 'C' ? 'btn btn-secondary btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('C') }}>°C</button>
-                    <button type="button" className={scale === 'F' ? 'btn btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('F') }}>°F</button>
+
+        <div className="container">
+            <nav className="navbar justify-content-center navbar-light bg-light">
+                <NavLink className="navbar-brand" to="/">Zone Scheduler</NavLink>
+            </nav>
+            <div className="clearfix mt-3">
+                <Link to={`/create`}><button className="btn btn-sm btn-outline-secondary">Create Schedule</button></Link>
+                <div className="float-right">
+                    <div className="btn-group btn-group-sm" role="group" >
+                        <button type="button" className={scale === 'C' ? 'btn btn-secondary btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('C') }}>°C</button>
+                        <button type="button" className={scale === 'F' ? 'btn btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('F') }}>°F</button>
+                    </div>
                 </div>
             </div>
-        </nav>
+        </div>
     )
 }
 
