282fa6a2ef512d4c415ea3ba7f026656180d6503
Lasantha Basnayake <lasantha57@gmail.com>
2020-05-10 22:05:22 +0800

    add header with global scale switch

--- src/app/shared/header/Header.jsx
+++ src/app/shared/header/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+
+import { GlobalContext } from '../../../context/GlobalContext';
 
 import './Header.css';
 
 const Header = () => {
+
+    const { scale, changeScale } = useContext(GlobalContext);
+
+    const toggleScale = (selected) => {
+        if (scale !== selected) {
+            changeScale(selected);
+        }
+    }
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -14,6 +24,10 @@ const Header = () => {
             <div className="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
                 <div className="navbar-nav">
                     <Link to={`/create`}><button className="btn btn-outline-secondary mr-2">Create Schedule</button></Link>
+                </div>
+                <div className="btn-group btn-group ml-2" role="group" >
+                    <button type="button" className={scale === 'C' ? 'btn btn-secondary btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('C') }}>°C</button>
+                    <button type="button" className={scale === 'F' ? 'btn btn-secondary active' : 'btn btn-secondary'} onClick={() => { toggleScale('F') }}>°F</button>
                 </div>
             </div>
         </nav>
